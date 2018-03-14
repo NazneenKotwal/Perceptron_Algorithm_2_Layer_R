f1aea1b58da42ce4431a6185e032ec59f5f12b04
NazneenKotwal <37354868+NazneenKotwal@users.noreply.github.com>
2018-03-13 22:18:56 -0400

    Initial commit

---
+++ 2_layer_neural_network.R
@@ -0,0 +1,34 @@
+library(MASS)
+library(nnet)
+
+set.seed(10)
+
+# Generate the data suggested (basically the XOR data set):
+# Since x belongs to [0,1] we sample it using Uniform Distribution
+nTrain = 25
+X      = runif( nTrain )
+X      = sort(X)
+X      = data.frame(X)
+YTrain = 0.3 + 0.2 * cos( 2 * pi * X ) 
+
+# Generate NTest new vectors and predict their response: 
+XTest = seq(0.,1.,length=50)
+XTest = sort( XTest )
+XTest = data.frame( XTest ) 
+YTest = 0.3 + 0.2 * cos( 2 * pi * XTest ) 
+
+# Train a neural network on this data
+regNet = nnet( X, YTrain, size=2, trace=FALSE ) # the example calling sequence
+YPredict = predict( regNet, XTest ) # the predictions
+
+matplot( X, YTrain, xlab='x', ylab='y', type="b", pch=18, col="black" )
+matplot( XTest, YPredict, type="b", pch=19, col="red", add=T ) 
+matplot( XTest, YTest, type="b", pch=20, col="green", add=T ) # the truth 
+
+legend( 0.5, 0.45, legend=c( "training data", "testing data", "truth" ), pch=c(18,19,20), col=c("black","red","green") )
+
+if( savePlots ){
+  dev.off()
+}
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+Two Layer Perceptron Algorithm as a part of ECE 759 Pattern Recognition
